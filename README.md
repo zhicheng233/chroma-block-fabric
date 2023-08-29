78e6f139d487dde81d095e98c64270f4a6fea96e
zhi_cheng233 <2473367376@qq.com>
2023-08-30 00:59:19 +0800

    feat: Welcome news refactor: To a certain extent to solve the impact of ambient light occlusion

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 
 # Chroma Block Fabric
+ [EN](#1)   [中文](#2)  [Donator/赞助名单❤️](#3)
 ![](https://zhicheng233.github.io/Software-data/chroma-block-fabric/LOGO.png)
 ![](https://cdn.modrinth.com/data/kjdTcQ9X/images/8644713ffba6cde80a366e64fba5bfcd90a3ead5.png)
 ![](https://zhicheng233.github.io/Software-data/chroma-block-fabric/2.png)
-# EN (中文在下面)
+<h2 id="1">EN</h2>
 Chroma Block for Minecraft Fabric High for Minecraft Artistic Videos
 
 * 🎨8 colors to choose from
@@ -21,13 +22,16 @@ Chroma Block Fabric is not compatible with iris! Please use the solution of Opti
 
 Chroma Block Fabric is not compatible with iris! Please use the solution of OptiFabric + OptiFine!
 
+~~After installing OptiFine, the shaders will display correctly. If you want to use Chroma Block without enabling shaders, please uninstall OptiFine!~~
+Since version 1.1.0, it is recommended to use OptiFine, even without shaders enabled. This can temporarily resolve the issue of Chroma Blocks being affected by ambient occlusion.
+
 Some shaders may require disabling or reducing the BOOM effect, otherwise, the following situation may occur:
 ![](https://zhicheng233.github.io/Software-data/chroma-block-fabric/2023-07-22_16.44.10.png)
 
 However, with the correct settings, unexpected effects can be achieved:
 ![](https://zhicheng233.github.io/Software-data/chroma-block-fabric/2023-07-22_16.43.57.png)
 
-# CN
+<h2 id="2">中文</h2>
 适用于Minecraft Fabric高版本的色度方块，用于Minecraft艺术类视频的创作
 
 * 🎨8种颜色可供选择
@@ -45,9 +49,19 @@ Chroma Block Fabric 与 iris 不兼容！请使用OptiFabric+OpitFine的解决�
 
 Chroma Block Fabric 与 iris 不兼容！请使用OptiFabric+OpitFine的解决方法！
 
+~~安装OpitFine后打开光影才会正常显示 如果您想在不启用光影的情况下使Chroma Block正常显示，请卸载OpitFine!~~
+自1.1.0版本后推荐使用OpitFine，即使在不开光影的情况下，这可以暂时解决Chroma Block受到环境光遮蔽的影响的问题
+
 部分光影需要关闭/降低BOOM效果否则会出现下面的状况:
 ![](https://zhicheng233.github.io/Software-data/chroma-block-fabric/2023-07-22_16.44.10.png)
 
 
 当然通过正确地设置可以达到意想不到的效果:
 ![](https://zhicheng233.github.io/Software-data/chroma-block-fabric/2023-07-22_16.43.57.png)
+
+
+<h2 id="3">Donator❤️</h2>
+
+### Thank you for your support
+
+ATC_rock 30￥
--- src/main/java/cn/zhicheng233/chromablockfabric/MainMod.java
+++ src/main/java/cn/zhicheng233/chromablockfabric/MainMod.java
@@ -1,27 +1,34 @@
 package cn.zhicheng233.chromablockfabric;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralTextContent;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class MainMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final String modid = "chroma-block-fabric";
+	public static boolean welcome = true;
 	public static final Logger LOGGER = LoggerFactory.getLogger(modid);
 
 	//新方块
@@ -50,6 +57,34 @@ public class MainMod implements ModInitializer {
 	//注册
 	@Override
 	public void onInitialize() {
+		// 注册事件监听器
+
+		ServerTickEvents.START_WORLD_TICK.register((entity) -> {
+
+			if (welcome){
+
+				// 获取玩家列表
+				List playerList = entity.getPlayers();
+				if (playerList != null && playerList.size() > 0) {
+					PlayerEntity player = (PlayerEntity) playerList.get(0);
+					// 发送聊天栏消息
+					player.sendMessage(Text.of("§a§l Donator:"));
+					player.sendMessage(Text.of("§eATC_rock 30￥"));
+					player.sendMessage(Text.of("§l----------------------------"));
+					player.sendMessage(Text.of("§e欢迎使用Chroma Block Fabric"));
+					welcome = false;
+				}
+
+
+
+
+				//player.sendMessage(Text.of("§c§l§n警告!您正在使用BETA版本这通常意味着该版本为 过渡版本 或者其功能是以较为 扭曲 的方式实现的，同时没有经过大规模的 生产环境 测试，可能会导致 不兼容 的情况!"));
+
+			}
+
+
+		});
+
 		//添加物品到物品组
 		ItemGroupEvents.modifyEntriesEvent(CBF_GROUP_KEY).register(content -> {
 			content.add(CBF_Green);
